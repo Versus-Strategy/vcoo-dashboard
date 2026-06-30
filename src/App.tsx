af898cfc3fc5151fd39ab9b901e3e344aa4765a0
MAGI-VERSUS <magi.versus@gmail.com>
2026-06-30 16:32:16 +0200

    feat: initialize Vite React TypeScript project with Tailwind and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen">
+        {/* Las rutas se agregarán aquí */}
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
